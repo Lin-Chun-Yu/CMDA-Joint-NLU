aa20eb6e08dd11f968599d6103d3e75ea36b2fe9
ChunYu Lin <75929785+Lin-Chun-Yu@users.noreply.github.com>
2026-05-08 19:49:15 +0800

    Add files via upload

---
+++ Joint_model/mix/cmda/data_augmentation/SP/augmenter.py
@@ -0,0 +1,146 @@
+# encoding: utf-8
+
+"""
+Created by Gözde Gül Şahin
+20.05.2018
+Flipper (Rotatation) and Cropper classes
+
+"""
+__author__ = 'Gözde Gül Şahin'
+
+from data_augmentation.SP.chunker import *
+import numpy
+class rotator:
+    def __init__(self, asent, aloi = [u"nsubj", u"dobj", u"iobj", u"obj", u"obl"],
+                                pl = u"root",
+                         multilabs = [u"case", u"fixed", u"flat", u"cop", u"compound"],
+                              prob = 0.3):
+
+        self.sent = asent
+        self.cnkr = chunker(asent, aloi, pl, multilabs)#深入了解
+        self.flex_chunks = self.cnkr.get_all_chunks()
+        self.threshold = int(prob*10)
+
+    def _reorder(self, chunk_order):#一個代表片段順序的列表 chunk_order，並根據這個順序將句子中的標記重新排列
+        """
+        Only insert interesting chunks - get rid of adverbial stuff
+        :param chunk_order: order of the chunks (0,2,1)...
+        :return: new index list
+        """
+        chnks = self.flex_chunks
+        # the new order
+        nlst = [0]
+        for chunk_id in chunk_order:
+            chnk = chnks[chunk_id]
+            # add nodes in order
+            for j in range(chnk.min, chnk.max+1):
+                nlst.append(j)
+        return nlst
+
+    def rotate(self, fields_order, texts, slots):
+        """
+        Shuffle the sentence by moving the flexible chunks around
+        :param maxshuffle: Maximum number of shuffles per sentence
+        :return: shuffled sentences + original sentences
+        """
+        shufledSents, shufledSents_slot = [], []
+
+        # if there are enough flexible chunks
+        if len(self.flex_chunks) > 1:#已經有大於0了
+
+            # permlst = list(itertools.permutations(range(len(self.flex_chunks))))# 生成所有可能的排列
+            num_shuffles = perm(len(self.flex_chunks))#flex_chunks數量!
+            permlst = list(itertools.permutations(range(len(self.flex_chunks))))
+            poss_perms = random.sample(permlst, num_shuffles)#itertools.permutations 生成所有可能的排列，並隨機選擇 num_shuffles 個排列。
+
+            for chunk_order in poss_perms:
+                num = numpy.random.choice(numpy.arange(1, 11))# 生成 1 到 10 之間的一個隨機數
+
+                if num <= self.threshold:# 如果隨機數小於等於閾值，就進行資料擴增
+                    neworder = self._reorder(chunk_order)# 重新排序區塊
+                    sen_order=[]
+                    for i, j in zip(neworder,range(len(neworder))):
+                        if i==0:
+                            continue
+                        sen_order.append(fields_order[i-1][0])
+                    seen = set()
+                    unique_order = [x for x in sen_order if x not in seen and not seen.add(x)]
+                    new_texts, new_slots = self.sent.reorder(unique_order, texts, slots)# 根據新順序重新排序句子和槽位
+                    shufledSents.append(new_texts)# 將新句子添加到結果列表中
+                    shufledSents_slot.append(new_slots)# 將新槽位添加到結果列表中
+
+        return shufledSents, shufledSents_slot
+
+
+class cropper:
+    def __init__(self, asent, aloi = [u"nsubj", u"dobj", u"iobj", u"obj", u"obl"],
+                                pl = u"root",
+                         multilabs = [u"case", u"fixed", u"flat", u"cop", u"compound"],
+                              prob = 0.3):
+
+        self.sent = asent
+        self.cnkr = chunker(asent, aloi, pl, multilabs)
+        self.flex_chunks = self.cnkr.get_all_chunks()
+        self.threshold = int(prob*10)
+
+    def _reorder(self, chunk_order):
+        """
+        Only insert interesting chunks - get rid of adverbial stuff
+        :param chunk_order: order of the chunks (0,2,1)...
+        :return: new index list
+        """
+        chnks = self.flex_chunks
+        # the new order
+        nlst = [0]
+        for chunk_id in chunk_order:
+            chnk = chnks[chunk_id]
+            # add nodes in order
+            for j in range(chnk.min, chnk.max+1):
+                nlst.append(j)
+        return nlst
+
+    def _get_root_chunk_ix(self):
+        """
+        Get the index of the root chunk
+        :return: index of root chunk
+        """
+        for i, cnk in enumerate(self.flex_chunks):
+            if cnk.type==u"root":
+               return i
+
+
+    def crop(self, fields_order, texts, slots):
+        """
+        Crop the sentence into meaningful small sentences
+        :return: cropped sentences + original sentences
+        """
+        croppedSents, croppedSents_slot = [], []
+
+        # if there are enough flexible chunks
+        if len(self.flex_chunks) > 1:# 如果有足夠的彈性區塊
+            # get the root chunk
+            root_ix = self._get_root_chunk_ix()# 獲取根區塊索引
+            for i in range(len(self.flex_chunks)):#遍歷每個靈活區塊。
+                if i==root_ix:#跳過根區塊。
+                    continue
+                elif i < root_ix:# 當前區塊在根區塊之前
+                    chunk_order = [i, root_ix]
+                elif i > root_ix:# 當前區塊在根區塊之後
+                    chunk_order = [root_ix, i]
+                # if above threshold
+                num = numpy.random.choice(numpy.arange(1, 11))# 生成1到10之間的一個隨機數
+                # augment data if we are above the probability threshold
+                if num <= self.threshold:# 如果隨機數小於等於閾值，則進行資料擴增
+                    neworder = self._reorder(chunk_order)
+                    sen_order=[]
+                    for i, j in zip(neworder,range(len(neworder))):
+                        if i==0:
+                            continue
+                        sen_order.append(fields_order[i-1][0])
+                    seen = set()
+                    unique_order = [x for x in sen_order if x not in seen and not seen.add(x)]
+                    new_texts, new_slots = self.sent.reorder(unique_order, texts, slots)# 根據新順序重新排序句子和槽位                   
+                    croppedSents.append(new_texts)
+                    croppedSents_slot.append(new_slots)
+
+        return croppedSents, croppedSents_slot
---
+++ Joint_model/mix/cmda/train.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-#
+#查看gpu指令watch nvidia-smi
+import os, json, random
+import numpy as np
+import torch
+from models.module import ModelManager  #自定義
+from utils.loader import DatasetManager #自定義
+from utils.process import Processor     #自定義
+from utils.config import *              #自定義
+
+
+
+if __name__ == "__main__":
+
+    #創建保存資料夾：代碼檢查是否存在一个指定的資料夾（args.save_dir），如果不存在，則使用系统命令創建該資料夾。
+    #這是用于儲存模型和訓練参数的資料夾。
+    if not os.path.exists(args.save_dir):
+        os.system("mkdir -p " + args.save_dir)
+
+    #保存參數到JSON檔案：程式碼將參數對象args中的參數保存為一個JSON文件（param.json），
+    #這個文件位於save資料夾中。這樣做可以記錄訓練和評估時所使用的參數配置。
+    log_path = os.path.join(args.save_dir, "param.json")
+    with open(log_path, "w", encoding="utf8") as fw:
+        fw.write(json.dumps(args.__dict__, indent=True))
+
+    #固定隨機種子：代碼固定了隨機種子，以确保訓練和評估的结果在不同運行之间是可重現的。
+    #這包括了Python的隨机種子和PyTorch庫在GPU和CPU上的隨機種子。
+    '''
+    random.seed(args.random_state)
+    np.random.seed(args.random_state)
+
+    # Fix the random seed of Pytorch when using GPU.
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(args.random_state)
+        torch.cuda.manual_seed(args.random_state)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+    # Fix the random seed of Pytorch when using CPU.
+    torch.manual_seed(args.random_state)
+    torch.random.manual_seed(args.random_state)
+    '''
+
+    #實例化數據集對象：使用DatasetManager類實例化了一个數據集對象（dataset），
+    #並進行了一些數據的快速建構和匯總。
+    dataset = DatasetManager(args)
+    dataset.quick_build()#讀取資料集，並做一些處理。
+    #exit()
+    dataset.show_summary()
+
+    #實例化模型对象：使用ModelManager類實例化了一個網路模型對象（model），
+    #並提供了輸入參數，包括詞彙表大小和意圖/槽位的數量。
+    #print(len(dataset.intent_alphabet))
+    #exit()
+    model = ModelManager(
+        args, len(dataset.word_alphabet),
+        len(dataset.slot_alphabet),
+        len(dataset.intent_alphabet)
+    )
+    model.show_summary()
+
+    #訓練和評估模型:
+    #使用Processor類實例化了一个處理器對象（process），並通過其train方法来訓練模型。在訓練過程中，最佳模型在每個epochs都会被保存
+    #使用Processor類的validate方法評估了訓練後的模型，並印出了在測試資料集上的性能指標。
+    process = Processor(dataset, model, args)
+    best_epoch = process.train()#這裡跑完就會得到最好的結果
+    results = Processor.validate(
+        os.path.join(args.save_dir, "model/model.pkl"),
+        dataset,
+        args.batch_size, len(dataset.intent_alphabet), args=args)
+    print('\nAccepted performance: ' + str(results) + " at test dataset;\n")
+
+    if not os.path.exists(args.log_dir):
+        os.makedirs(args.log_dir)
+    with open(os.path.join(args.log_dir, args.log_name), 'a') as fw:
+        fw.write(str(best_epoch))
+        for result in results:
+            fw.write(',' + str(result))
+        fw.write("\n")
